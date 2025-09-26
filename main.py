6dae0e010d586416dfb10a86b095d7894583c2d6
ashton231 <ashtonhuston642@gmail.com>
2025-09-25 20:13:22 -0400

    kagglehub

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello World')
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("borismarjanovic/price-volume-data-for-all-us-stocks-etfs")
+
+print("Path to dataset files:", path)
