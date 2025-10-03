7d673a7ee4750a7f31a169cec345103c5c3eab06
ashton231 <ashtonhuston642@gmail.com>
2025-10-02 20:06:00 -0400

    Etf in

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("borismarjanovic/price-volume-data-for-all-us-stocks-etfs")
-
-print("Path to dataset files:", path)
-print("67")
+import pandas as pd
+import numpy as np
+import Matplotlib
