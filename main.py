d90034ce178d38c73c6437443577c251899d040b
ashton231 <ashtonhuston642@gmail.com>
2025-10-02 20:24:30 -0400

    read first file in folders

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
-import Matplotlib
+import matplotlib
+
+dfETFs = pd.read_csv("ETFs/aadr.us.txt", sep=",")
+print(dfETFs.head())
+print(dfETFs.shape)
+
+dfStocks = pd.read_csv("Stocks/a.us.txt", sep=",")
+print(dfStocks.head())
+print(dfStocks.shape)
